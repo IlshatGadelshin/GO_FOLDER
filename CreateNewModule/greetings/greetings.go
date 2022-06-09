520807a28afa51e7c548edab003627326601b2ad
Ильшат Гадельшин <vinnie8warrior@gmail.com>
2022-06-09 14:15:37 +0500

    error handling added

--- CreateNewModule/greetings/greetings.go
+++ CreateNewModule/greetings/greetings.go
@@ -4,7 +4,7 @@ import "fmt"
 import "errors"
 
 // Hello returns a greeting for the named person.
-func Hello(name string) string{
+func Hello(name string) (string, error){
 	// If no name was given, return an error with a message.
 	if name==""{
 		return "", errors.New("empty Name!")
