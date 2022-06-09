383bba69644f6bd4c4d0ba474b5e546c80011ab8
Ильшат Гадельшин <vinnie8warrior@gmail.com>
2022-06-09 14:12:52 +0500

    error handling added

--- CreateNewModule/greetings/greetings.go
+++ CreateNewModule/greetings/greetings.go
@@ -1,11 +1,16 @@
 package greetings
 
 import "fmt"
+import "errors"
 
 // Hello returns a greeting for the named person.
 func Hello(name string) string{
+	// If no name was given, return an error with a message.
+	if name==""{
+		return "", errors.New("empty Name!")
+	}
 	var message string
 	// Return a greeting that embeds the name in a message.
 	message = fmt.Sprintf("Hi, %v. Welcome!", name)
-	return message
+	return message, nil
 }
