2b84060640a6df97b401cd6b0744e6b2dd727c81
Ильшат Гадельшин <vinnie8warrior@gmail.com>
2022-06-09 14:26:54 +0500

    Now this code enable randomly greet you!

--- CreateNewModule/greetings/greetings.go
+++ CreateNewModule/greetings/greetings.go
@@ -2,6 +2,8 @@ package greetings
 
 import "fmt"
 import "errors"
+import "math/rand"
+import "time"
 
 // Hello returns a greeting for the named person.
 func Hello(name string) (string, error){
@@ -11,6 +13,27 @@ func Hello(name string) (string, error){
 	}
 	var message string
 	// Return a greeting that embeds the name in a message.
-	message = fmt.Sprintf("Hi, %v. Welcome!", name)
+	message = fmt.Sprintf(randomFormat(), name)
 	return message, nil
+}
+
+// init sets initial values for variables used in the function.
+func init(){
+	rand.Seed(time.Now().UnixNano())
+}
+
+// randomFormat returns one of a set of greeting messages. The returned
+// message is selected at random.
+func randomFormat() string{
+	// A slice of message formats.
+	formats:=[]string{
+		"Hi, %v. Welcome!",
+        "Great to see you, %v!",
+        "Hail, %v! Well met!",
+	}
+	// Return a randomly selected message format by specifying
+    // a random index for the slice of formats.
+
+	return formats[rand.Intn(len(formats))]
+
 }
