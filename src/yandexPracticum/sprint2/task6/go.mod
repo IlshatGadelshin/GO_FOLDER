954e77c21a02c744d2ab1a9eef64e42f56748910
Ильшат Гадельшин <vinnie8warrior@gmail.com>
2022-07-03 18:37:52 +0500

    shows files in directory

---
+++ src/yandexPracticum/sprint2/task6/go.mod
@@ -0,0 +1,3 @@
+module yandexPracticum/sprint2/task6
+
+go 1.18
---
+++ src/yandexPracticum/sprint2/task6/main.go
@@ -0,0 +1,34 @@
+/*Приведём пример работы с рекурсивным обходом всех файлов в данной 
+директории, причём директория может содержать вложенные поддиректории:*/
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"path/filepath"
+)
+
+func main() {
+    PrintAllFiles(".")
+}
+
+func PrintAllFiles(path string) {
+    // получаем список всех элементов в папке (и файлов, и директорий)
+    files, err := ioutil.ReadDir(path)
+    if err != nil {
+        fmt.Println("unable to get list of files", err)
+        return
+    }
+    //  проходим по списку
+    for _, f := range files {
+        // получаем имя элемента
+        // filepath.Join — функция, которая собирает путь к элементу с разделителями
+        filename := filepath.Join(path, f.Name())
+        // печатаем имя элемента
+        fmt.Println(filename)
+        // если элемент — директория, то вызываем для него рекурсивно ту же функцию
+        if f.IsDir() {
+            PrintAllFiles(filename)
+        }
+    }
+}
